4d5d2e1f9f009234a8a1edf433870f2b872578e9
bguiz <bguiz@users.noreply.github.com>
2020-05-06 22:34:24 +0800

    step: 05-08: render component

--- app/src/App.js
+++ app/src/App.js
@@ -99,6 +99,61 @@ class App extends Component {
       });
   }
 
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <span className="navbar-brand col-sm-3 col-md-2 mr-0">
+            Colour Tokens
+          </span>
+
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white"><span id="account">{this.state.account}</span></small>
+            </li>
+          </ul>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <h1>Issue Token</h1>
+                <form onSubmit={(event) => {
+                  event.preventDefault();
+                  const colourStr = this.colour.value;
+                  this.mint(colourStr);
+                }}>
+                  <input
+                    type='text'
+                    className='form-control mb-1'
+                    placeholder='e.g. #FF00FF'
+                    ref={(input) => { this.colour = input }}
+                  />
+                  <input
+                    type='submit'
+                    className='btn btn-block btn-primary'
+                    value='MINT'
+                  />
+                </form>
+              </div>
+            </main>
+          </div>
+          <hr/>
+          <div className="row text-center">
+            { this.state.colours.map((colourStr, key) => {
+              return (
+                <div key={key} className="col-md-3 mb-3">
+                  <div className="token" style={{ backgroundColor: colourStr }}></div>
+                  <div>{colourStr}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
