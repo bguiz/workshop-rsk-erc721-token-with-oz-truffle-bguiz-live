7c70372560a6224f33ae78acb4409ca7e5449143
bguiz <bguiz@users.noreply.github.com>
2020-05-06 22:28:48 +0800

    step: 05-05: web3 init

--- app/src/App.js
+++ app/src/App.js
@@ -32,6 +32,23 @@ class App extends Component {
     await this.loadBlockchainData();
   }
 
+  async loadWeb3() {
+    if (window.ethereum) {
+      // current web3 providers
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      // fallback for older web3 providers
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      // no web3 provider, user needs to install one in their browser
+      window.alert(
+        'No injected web3 provider detected');
+    }
+    console.log(window.web3.currentProvider);
+  }
 }
 
 export default App;
